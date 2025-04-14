cd78f742e01a136abbc168c019811c42a11458a5
RiverDewberry <rde80630@oconeeschools.org>
2025-04-13 22:27:50 -0400

    added -i option

--- mk.asm
+++ mk.asm
@@ -6,6 +6,9 @@
 
 	mov rdx, rax	;stores rax
 
+	cmp rax, 0xffff_ffff_ffff_efff
+	jg _fileErr
+
 	mov rsi, [perms]
 	mov rax, 90
 	syscall		;sets perms
@@ -17,7 +20,20 @@
 	syscall		;closes the file
 %endmacro
 
+%macro checkOctal 1	;checks if char at offset is valid octal, if it is, shifts into rdx
+	mov al, [rdi + %1]
+	cmp al, 0x30
+	jl _optErr
+	cmp al, 0x37
+	jg _optErr	;checks if char at offset is valid octal
+	
+	shl rdx, 3
+	sub al, 0x30
+	or dl, al	;adds val to dl
+%endmacro
+
 %macro parseOptions 0	;parses command line args
+%%parseNext:
 	mov rdi, [rsp]	;gets the val of the first non path arg
 	mov al, [rdi]	;sets al to the first byte of the possible options arg
 	
@@ -36,47 +52,25 @@
 	cmp al, 'v'
 	je %%version	;prints version if -v
 
-	mov al, [rdi + 1]
-	cmp al, 0x30
-	jl _err
-	cmp al, 0x37
-	jg _err		;checks if first char is valid octal
+	cmp al, 'i'
+	je %%ignore	;stops parsing if -i
 
-	sub al, 0x30
-	or dl, al	;adds val to dl
-	
-	mov al, [rdi + 2]
-	cmp al, 0x30
-	jl _err
-	cmp al, 0x37
-	jg _err		;checks if next char is valid octal
-
-	shl rdx, 3
-	sub al, 0x30
-	or dl, al	;adds val to dl
-	
-	mov al, [rdi + 3]
-	cmp al, 0x30
-	jl _err
-	cmp al, 0x37
-	jg _err		;checks if final char is valid octal
-
-	shl rdx, 3
-	sub al, 0x30
-	or dl, al	;adds val to dl
+	checkOctal 1
+	checkOctal 2
+	checkOctal 3	;checks if all octal vals are valid
 
 	mov al, [rdi + 4]
 	cmp al, 0
-	jne _err	;errors if anything after -OOO
+	jne _optErr	;errors if anything after -OOO
 
 	mov [perms], rdx;sets perms to rdx
 
-	jmp %%endParse
+	jmp %%parseNext	;parses next arg
 
 %%version:		;prints the version num
 	mov al, [rdi + 2]
 	cmp al, 0
-	jne _err	;errors if anything after -v
+	jne _optErr	;errors if anything after -v
 
 	mov rax, 1
 	mov rdi, 1
@@ -89,22 +83,33 @@
 	
 	mov al, [rdi + 2]
 	cmp al, 0
-	jne _err	;errors if anything after -h
+	jne _optErr	;errors if anything after -h
 
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, helpMsg
-	mov rdx, 214
+	mov rdx, 300
 	syscall
 	jmp _exit
+
+%%ignore:		;stops parsing args
+
+	mov al, [rdi + 2]
+	cmp al, 0
+	jne _optErr	;errors if anything after -i
 
 %%endParse:
 %endmacro
 
 section .data
-	argError db "mk: Arguments invalid. Use -h for help.",0x0a
-	helpMsg db "Usage: [OPTION] FILENAME...",0x0a,"Create FILENAME(S), if they do not already exist",0x0a,0x0a,"Options:",0x0a,"  -OOO set file perms on created files to the octal OOO",0x0a,"  -h display this help msg and exit",0x0a,"  -v print version number and exit",0x0a
-	versionNum db "mk 0.0",0x0a
+	
+	optionError db "mk: invalid options.",0x0a,"Try 'mk -h' for a list of valid options.",0x0a
+	noOperandError db "mk: no operand.",0x0a,"Try 'mk -h' for more information.",0x0a
+	fileErr db "mk: cannot create file '"
+	fileErrEnd db "'.",0x0a
+	helpMsg db "Usage: [OPTION]... FILENAME...",0x0a,"Create FILENAME(S), if they do not already exist",0x0a,0x0a,"Options:",0x0a,"  -OOO set file perms on created files to the octal OOO",0x0a,"  -h display this help msg and exit",0x0a,"  -v print version number and exit",0x0a,"  -i mark end of OPTION(s), useful if you want to make a file that starts with '-'",0x0a
+	versionNum db "mk 0.1",0x0a
+	
 	perms dw 0o644
 
 section .text
@@ -117,12 +122,12 @@ _start:
 	dec rbx		;sets counter to new number of args
 
 	cmp rbx, 1
-	jl _err		;ends program if 0 args
+	jl _noOpErr	;ends program if 0 args
 
 	parseOptions	;parses options (if any)
 
 	cmp rbx, 1
-	jl _err		;ends program if 0 file args
+	jl _noOpErr	;ends program if 0 file args
 
 _argLoop:		;loops through all args
 	pop rdi		;file to be created
@@ -138,13 +143,55 @@ _exit:
 	mov rdi, 0
 	syscall		;exits program with err 0
 
-_err:			;exits with an error
+_fileErr:		;if file can not be created
+	
+	xor rax, rax
+
+_fileErrLoop:
+	inc rax
+	mov dl, [rdi + rax]
+	cmp dl, 0
+	jne _fileErrLoop;finds size of file name
+	
+	mov rbx, rax	;stores name length
+	mov r8, rdi	;stores name ptr
+
 	mov rax, 1
 	mov rdi, 1
-	mov rsi, argError
-	mov rdx, 40
+	mov rsi, fileErr
+	mov rdx, 24
+	syscall
+
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, r8
+	mov rdx, rbx
+	syscall
+
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, fileErrEnd
+	mov rdx, 3
+	syscall		;prints error
+
+	jmp _err
+
+_noOpErr:		;if no operand if given
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, noOperandError
+	mov rdx, 50
+	syscall		;prints error mesage
+	jmp _err	;exits with error
+
+_optErr:		;if invalid options
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, optionError
+	mov rdx, 62
 	syscall		;prints error mesage
 
+_err:			;exits with an error
 	mov rax, 60
 	mov rdi, 1
 	syscall		;exits with err 1
